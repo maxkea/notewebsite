49fc39771775e622c4227d7e841b136156e6aeb2
maxx <luumaxx123@gmail.com>
2026-07-30 15:22:25 +0700

    add loki alloy grafana

--- backend/logger.js
+++ backend/logger.js
@@ -1,13 +1,21 @@
 const pino = require('pino');
+const path = require('path');
 
-const logger = pino({
+const logFilePath = path.join(__dirname, 'logs', 'backend_log.log');
+const logger = pino(
+  {
   timestamp: pino.stdTimeFunctions.isoTime, 
   formatters: {
     level(label) {
       return { level: label }; 
     },
-  },
+    },
   base: { service: 'node_backend' }, 
-});
+  },
+  pino.multistream([
+    { stream: process.stdout }, // Vẫn in ra màn hình console
+    { stream: pino.destination({ dest: logFilePath, mkdir: true, sync: false }) } // Ghi vào file tùy tên
+  ])
+);
 
 module.exports = logger;
